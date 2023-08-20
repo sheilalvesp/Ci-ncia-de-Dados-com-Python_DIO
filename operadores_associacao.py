0ee2ee6a64d2ec1d55e4cc5fc7b8a8de5331ac3b
sheilalvesp <alves.sheilap@gmail.com>
2023-08-20 10:04:26 -0300

    modified:   operadores_associacao.py 	modified:   operadores_identidade.py

--- operadores_associacao.py
+++ operadores_associacao.py
@@ -4,3 +4,8 @@ cursos= "Curso de Python"
 print("laranja" in frutas)
 print("limao"  in frutas)
 print("Python" in cursos)
+
+
+saldo = 500
+saldo += 300
+print(saldo)
--- operadores_identidade.py
+++ operadores_identidade.py
@@ -5,3 +5,5 @@ print(saldo is limite)
 print(saldo is not limite)
 
 
+x = (22 - 10) * 3
+print(x)
