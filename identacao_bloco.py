a0a590fe07f1e3469af228c171acc5bab209e93e
sheilalvesp <alves.sheilap@gmail.com>
2023-08-20 11:15:00 -0300

    new file:   estrutura_condicional_aninhada.py 	new file:   estrutura_condicional_ternaria.py 	new file:   estruturas_condicionais.py 	new file:   identacao_bloco.py

---
+++ identacao_bloco.py
@@ -0,0 +1,14 @@
+def sacar(valor):
+    saldo =500
+    if saldo >= valor:
+        print("valor sacado")
+        print("retire seu dinheiro na boca do caixa")
+
+print("Obrigada por ser nosso cliente!")
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+
+sacar(100)
