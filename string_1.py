8bfc6ea8cef6d13d6a2dbb9b545784ae4f969210
sheilalvesp <alves.sheilap@gmail.com>
2023-08-20 19:50:53 -0300

    new file:   string_1.py 	new file:   string_2.py 	new file:   string_3.py 	new file:   string_4.py

---
+++ string_1.py
@@ -0,0 +1,21 @@
+nome ="sHeiLA"
+
+print(nome.upper()) #maiusculo
+print(nome.lower()) #minusculo
+print(nome.title()) #primeira letra maiuscula
+
+
+texto = "  Olá Mundo    "
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() +".")
+print(texto.lstrip()+ ".")
+
+
+menu = "Python"
+
+print("###" + menu + "###")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
+
